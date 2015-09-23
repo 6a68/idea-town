3299ac0dbee90a233dc7f0476975944b28b3cffc
Jared Hirsch <ohai@6a68.net>
2015-09-22 18:17:00 -0700

    Inline the new landing page templates just for the moment

--- idea_town/frontend/static-src/app/views/landing-page.js
+++ idea_town/frontend/static-src/app/views/landing-page.js
@@ -9,22 +9,47 @@ const addonInfo = {
 };
 
 export default BaseView.extend({
-  _template: `<section class="page {{#loggedIn}} loggedIn {{/loggedIn}}">
-                {{^loggedIn}}
-                  <h1>Introducing Idea Town</h1>
-                  <p>We are building the next generation of Firefox features
-                     and we want your feedback!
-                  <p>Get started with a Firefox Account.
-                  <a href="/accounts/login/?next=/home">Sign Up</button>
-                {{/loggedIn}}
-                {{#loggedIn}}
-                  <h1>Thanks for Signing Up!</h1>
-                  <p>Install the Idea Town add-on to participate in experiments
-                     and give us feedback.
-                  <a href="{{ downloadUrl }}">Install the Add-on</button>
-                {{/loggedIn}}
-              </section>`,
-
+  _template: `
+            {{^loggedIn}}
+              <div class="sign-up" id="cta">
+                <div id="main-content">
+                  <div id="tabzilla">
+                    <a href="https://www.mozilla.org/">Mozilla</a>
+                  </div>
+                  <div class="firefox-logo"></div>
+                  <h1 class="hero">Introducing Idea Town</h1>
+                  <h2 class="sub-hero">We're building the next generation of<br> Firefox features and we want your feedback! <br>Get started with a Firefox Account.</h2>
+                  <div class="cta-layout-wrapper">
+                    <div class="cta-layout">
+                      <a href="/accounts/login/?next=/home"><button class="button large primary">Sign up</button></a>
+                      <a href="/accounts/login/?next=/home" class="fxa-alternate">Already have an account? Sign in.</a>
+                      <p class="cta-legal">By proceeding, you agree to the Terms of Service and Privacy Notice of Idea Town.</p>
+                    </div>
+                    <div class="town"></div>
+                  </div>
+                </div>
+              </div>
+            {{/loggedIn}}
+            {{#loggedIn}}
+              <div class="add-on" id="cta">
+                <div id="main-content">
+                  <div id="tabzilla">
+                    <a href="https://www.mozilla.org/">Mozilla</a>
+                  </div>
+                  <div class="firefox-logo"></div>
+                  <h1 class="hero">Thanks for Signing up!</h1>
+                  <h2 class="sub-hero">Install the Idea Town Add-on to participate<br> in experiments and give us feedback<br></h2>
+                  <div class="cta-layout-wrapper">
+                    <div class="cta-layout">
+                      <a href="{{ downloadUrl }}"><button class="button large primary">Install the Add-on</button></a>
+                    </div>
+                    <div class="town"></div>
+                  </div>
+                </div>
+                <div id="footer">
+                </div>
+            {{/loggedIn}}
+              `,
   render() {
     this.loggedIn = !!app.me.session;
     this.downloadUrl = addonInfo.url;
